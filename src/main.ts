3300fa5d582b5497454efff06d9db47ddf85e349
sohseyedi <sohseyedi@gmail.com>
2024-12-26 09:50:52 +0330

    feat:initial project

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import { router } from "./routes";
+
+createApp(App).use(router).use(createPinia()).mount("#app");
