3af95aeee9b74f8d318d2af4321c9ecdeab7e708
sohseyedi <sohseyedi@gmail.com>
2024-12-26 09:52:25 +0330

    feat:add toast notification and set up main application layout

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Toaster } from 'vue-sonner'
+</script>
+
+<template>
+  <section class="max-w-2xl px-4 mx-auto min-h-screen bg-[#161616] relative">
+    <Toaster position="top-right" rich-colors />
+    <router-view />
+  </section>
+</template>
