43cad5133695d6116a129178759f2cce7f8c2b49
sohseyedi <sohseyedi@gmail.com>
2024-12-28 13:02:33 +0330

    feat:create resusable loading component

---
+++ src/components/common/Loading.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex items-center space-x-2">
+    <span class="sr-only">Loading...</span>
+    <div class="size-3 bg-[#161616] rounded-full animate-bounce [animation-delay:-0.3s]" />
+    <div class="size-3 bg-[#161616] rounded-full animate-bounce [animation-delay:-0.15s]" />
+    <div class="size-3 bg-[#161616] rounded-full animate-bounce" />
+  </div>
+</template>
