c09a664edda9d4bceac2b0164761b30dba46ae48
sohseyedi <sohseyedi@gmail.com>
2024-12-28 13:13:23 +0330

    feat:create home header

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
+
+<template>
+  <header class="flex items-center justify-between py-5">
+    <button class="size-10 rounded-2xl bg-black flex items-center justify-center">
+      <Icon icon="solar:user-broken" class="text-[#fff] size-7 cursor-pointer" />
+    </button>
+    <div class="flex items-center gap-2">
+      <button class="">
+        <Icon icon="solar:wallet-2-bold" class="text-[#a3e632] size-10 cursor-pointer" />
+      </button>
+      <router-link to="/all">
+        <button class="size-10 rounded-2xl bg-black flex items-center justify-center">
+        <Icon icon="solar:magnifer-outline" class="text-[#fff] size-6 cursor-pointer" />
+      </button>
+      </router-link>
+    </div>
+  </header>
+</template>
