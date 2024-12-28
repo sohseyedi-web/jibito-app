5fcf84665dea77119a8ce1065c958874bf9768ab
sohseyedi <sohseyedi@gmail.com>
2024-12-28 13:06:56 +0330

    feat:back button with icon for navigation

---
+++ src/components/common/BackButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const onBack = () => router.back()
+</script>
+
+<template>
+  <button class="rounded-2xl bg-[#202020]  cursor-pointer" @click="onBack">
+    <Icon icon="mdi:chevron-right" class=" text-[#a3e632] size-10" />
+  </button>
+</template>
