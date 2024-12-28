30c0c65aab50b39aaefa6af2a1ec4f863af23e74
sohseyedi <sohseyedi@gmail.com>
2024-12-28 13:13:54 +0330

    feat:transaction details with localized date and formatted amount

---
+++ src/components/base/AppTransactionItem.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { TransactionType } from '../../types/storeTypes';
+import { toPersianNumbersWithComma as TPNWC } from '../../utils/toPersianNumbers';
+import CategoryIcon from '../common/CategoryIcon.vue'
+import {useI18n} from 'vue-i18n'
+
+defineProps<{ transaction: TransactionType }>()
+const {t} = useI18n()
+
+function onConvertShort(date?: string) {
+  const validDate = date && !isNaN(new Date(date).getTime()) ? new Date(date) : new Date();
+
+  return validDate.toLocaleDateString('fa-IR', { day: 'numeric', month: 'short' });
+}
+
+
+</script>
+
+<template>
+  <div :key="transaction.id" class="flex items-center justify-between bg-black p-2 rounded-2xl my-2">
+    <div class="flex items-center gap-x-2">
+      <CategoryIcon :category="transaction.category" />
+      <div class="text-white">
+        <h5 class="text-lg font-semibold">
+          {{ t(`category.${transaction.category}`) }}
+        </h5>
+        <span class="text-gray-500 text-sm">{{ transaction.description || "توضیحی وارد نشده" }}</span>
+      </div>
+    </div>
+    <div class="flex flex-col items-center">
+      <span class="text-xl font-semibold" :class="transaction.type === 'Income' ? 'text-green-600' : 'text-red-600'">
+        {{ transaction.type === 'Income' ? '+' : '-' }} {{ TPNWC(transaction.amount) }}
+      </span>
+      <span class="text-gray-500">{{ onConvertShort(transaction.date) }}</span>
+    </div>
+  </div>
+</template>
