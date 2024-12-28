47eeeebfd2eeeade24a80abb63f111a4feb1f5e6
sohseyedi <sohseyedi@gmail.com>
2024-12-28 13:11:56 +0330

    feat:create deposit functionality with form validation and success toast

---
+++ src/components/AmountForm.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { computed, provide, ref, shallowRef } from 'vue'
+import AppButton from './base/AppButton.vue'
+import AppForm from './base/AppForm.vue'
+import AppInput from './base/AppInput.vue'
+import {useTransactionStore} from '../store/useStore'
+import useValid , {amountSchema} from '../composable/useValid'
+import {showToast} from '../utils/showToast'
+import type { LoadingMode } from '../types/commonTypes'
+
+const { addTransaction,getTransactions } = useTransactionStore()
+const isLoading = ref<LoadingMode>('INITIAL')
+const isSubmitted = shallowRef<boolean>(false)
+
+const transactions = computed(() => getTransactions().sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()))
+
+const { errors, validateForm, values } = useValid({ amount: '' }, amountSchema)
+
+async function onSubmit() {
+  isLoading.value = 'LOADING'
+  const isValid = validateForm()
+
+  if (!isValid) {
+    isSubmitted.value = true
+    isLoading.value = 'FAILED'
+  }
+
+  try {
+    if (isValid) {
+      addTransaction({
+        amount: values.value.amount,
+        category: 'Income',
+        date: new Date().toString(),
+        type: 'Income',
+        wallet: 'کیف 1',
+        description: 'موجودی اولیه',
+      })
+      showToast('success', 'موجودی افزایش یافت')
+      isLoading.value = 'RESOLVED'
+    }
+  }
+  catch {
+    // showerror
+    // console.log(ex)
+    isLoading.value = 'FAILED'
+  }
+  finally {
+    isLoading.value = 'INITIAL'
+  }
+}
+provide('isSubmitted', isSubmitted)
+</script>
+
+<template>
+  <AppForm @submit="onSubmit" class="text-white space-y-3 lg:w-[75%] w-[95%] mx-auto flex items-center justify-center h-screen">
+    <h2 class="text-xl font-semibold">افزایش موجودی</h2>
+    <AppInput
+      v-model="values.amount"
+      :error="errors.amount"
+      type="text"
+      placeholder="مبلغ"
+      icon="solar:dollar-broken"
+    />
+    <AppButton title="ثبت مبلغ" type="submit" :loading="isLoading" class="bg-[#a3e632]"/>
+    <router-link v-if="transactions.length" to="/all" class="w-full">
+      <AppButton title="تاریخچه حساب" type="button" class="text-white bg-[#4C585B]"/>
+    </router-link>
+  </AppForm>
+</template>
