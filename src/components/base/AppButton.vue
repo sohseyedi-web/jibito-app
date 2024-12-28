f0deb269f4d2c283ff4ed29bffaebaeb4bd47c60
sohseyedi <sohseyedi@gmail.com>
2024-12-28 13:03:56 +0330

    feat:create resusable button component

---
+++ src/components/base/AppButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { LoadingMode } from '../../types/commonTypes'
+import Loading from '../common/Loading.vue'
+
+const { width = '100%' } = defineProps<{
+  title: string
+  type: 'submit' | 'button' | 'reset'
+  width?: string
+  loading?: LoadingMode
+}>()
+</script>
+
+<template>
+  <button
+    :type
+    class="text-[#161616] font-semibold text-lg w-full h-[55px] rounded-2xl flex items-center justify-center cursor-pointer border-none"
+    :style="{ width }"
+  >
+    <Loading v-if="loading === 'LOADING'" />
+    <span v-else>{{ title }}</span>
+  </button>
+</template>
