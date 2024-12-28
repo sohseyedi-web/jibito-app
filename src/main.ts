9bfc1ea89954fd78993c3e6da17bc9dd6b132f46
sohseyedi <sohseyedi@gmail.com>
2024-12-28 13:02:09 +0330

    chore:configure vue-i18n for multilingual support

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,17 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import { router } from "./routes";
+import { createI18n } from "vue-i18n";
+import en from "./utils/translate/en.json";
+import fa from "./utils/translate/fa.json";
 
-createApp(App).use(router).use(createPinia()).mount("#app");
+const i18n = createI18n({
+  locale: "fa",
+  fallbackLocale: "en",
+  messages: {
+    fa,
+    en,
+  },
+});
+
+createApp(App).use(router).use(i18n).use(createPinia()).mount("#app");
