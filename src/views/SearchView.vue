9dc4dd828aa6fba6fea0f959c0668affb3b3211a
sohseyedi <sohseyedi@gmail.com>
2024-12-29 10:46:34 +0330

    chore:transition group for show items

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -4,18 +4,26 @@ import { computed, ref } from 'vue'
 import AppTransactionItem from '../components/base/AppTransactionItem.vue'
 import BackButton from '../components/common/BackButton.vue'
 import { useTransactionStore } from '../store/useStore'
+import { useI18n } from 'vue-i18n'
 
 const { getTransactions } = useTransactionStore()
-
 const searchValue = ref<string>('')
+const {t} = useI18n()
+
+const translateSearchQuery = (query : string) => {
+  return t(`category.${query}`);
+};
 
 const transactions = computed(() => {
   const allTransactions = getTransactions().sort((a, b) =>
-    new Date(a.date).getTime() - new Date(b.date).getTime(),
+    new Date(b.date).getTime() - new Date(a.date).getTime(),
   )
 
   return allTransactions.filter((transaction) => {
     const query = searchValue.value.toLowerCase()
+    const reqqq = translateSearchQuery(query)
+    console.log(reqqq)
+
 
     return (
       transaction.amount.toString().includes(query)
@@ -38,5 +46,44 @@ const transactions = computed(() => {
       >
     </div>
   </div>
-  <AppTransactionItem v-for="transaction in transactions" :key="transaction.id" :transaction="transaction" />
+  <Transition
+    enter-from-class="opacity-50"
+    leave-to-class="opacity-50"
+    enter-active-class="duration-200 ease-in"
+    leave-active-class="duration-200 ease-out"
+    mode="out-in"
+  >
+    <p v-if="!transactions.length" class="text-white text-xl font-semibold text-center">
+      تراکنشی پیدا نشد
+    </p>
+    <TransitionGroup
+      v-else
+      tag="div"
+      class="space-y-4"
+      name="fade"
+    >
+      <AppTransactionItem
+        v-for="transaction in transactions"
+        :key="transaction.id"
+        :transaction="transaction"
+      />
+    </TransitionGroup>
+  </Transition>
 </template>
+
+<style scoped>
+/* تعریف افکت‌های ترنزیشن */
+.fade-enter-active, .fade-leave-active {
+  transition: all 0.5s ease-in;
+}
+
+.fade-enter-from {
+  opacity: 50;
+  transform: translateY(20px);
+}
+
+.fade-leave-to {
+  opacity: 50;
+  transform: translateY(-20px);
+}
+</style>
