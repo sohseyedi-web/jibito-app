b91100303d417cbd35b25055e84b9362bd3c40f3
sohseyedi <sohseyedi@gmail.com>
2024-12-29 10:44:35 +0330

    style:lint app

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import { createPinia } from "pinia";
-import { createApp } from "vue";
-import { createI18n } from "vue-i18n";
-import App from "./App.vue";
-import { router } from "./routes";
-import en from "./utils/translate/en.json";
-import fa from "./utils/translate/fa.json";
-import "./style.css";
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+import App from './App.vue'
+import { router } from './routes'
+import en from './utils/translate/en.json'
+import fa from './utils/translate/fa.json'
+import './style.css'
 
 const i18n = createI18n({
-  locale: "fa",
-  fallbackLocale: "en",
+  locale: 'fa',
+  fallbackLocale: 'en',
   messages: {
     fa,
     en,
   },
-});
+})
 
-createApp(App).use(router).use(i18n).use(createPinia()).mount("#app");
+createApp(App).use(router).use(i18n).use(createPinia()).mount('#app')
