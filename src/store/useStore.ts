f9c3402c85833ce3ff5da9a78347d082cd50a3b7
sohseyedi <sohseyedi@gmail.com>
2024-12-30 10:26:34 +0330

    chore:sort items and change remove items handler

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,39 +1,39 @@
-import type { TransactionType } from "../types/storeTypes";
-import { defineStore } from "pinia";
-import { ref } from "vue";
+import type { TransactionType } from '../types/storeTypes'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useTransactionStore = defineStore("transaction", () => {
-  const transactions = ref<TransactionType[]>([]);
+export const useTransactionStore = defineStore('transaction', () => {
+  const transactions = ref<TransactionType[]>([])
 
   const generateId = () => {
-    return Date.now().toString(36) + Math.random().toString(36).substring(2);
-  };
+    return Date.now().toString(36) + Math.random().toString(36).substring(2)
+  }
 
-  const addTransaction = (transaction: Omit<TransactionType, "id">) => {
+  const addTransaction = (transaction: Omit<TransactionType, 'id'>) => {
     const newTransaction = {
       id: generateId(),
       ...transaction,
-    };
+    }
 
-    transactions.value.push(newTransaction);
-  };
+    transactions.value.push(newTransaction)
+  }
 
   const removeTransaction = (id: string) => {
     transactions.value = transactions.value.filter(
-      (transaction) => transaction.id !== id
-    );
-  };
+      transaction => transaction.id !== id,
+    )
+  }
 
   const getTransactions = () => {
     return transactions.value.sort(
-      (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
-    );
-  };
+      (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
+    )
+  }
 
   return {
     transactions,
     addTransaction,
     removeTransaction,
     getTransactions,
-  };
-});
+  }
+})
