ecdd68e4c8da8b329a87cb24f6934c3fde996cab
sohseyedi <sohseyedi@gmail.com>
2024-12-31 12:32:36 +0330

    chore:authentication guards

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,73 +1,115 @@
+import { createRouter, createWebHistory } from "vue-router";
+// vue router types
 import type {
-  NavigationGuardNext,
-  RouteLocationNormalized,
   RouteRecordRaw,
-} from 'vue-router'
-import { createRouter, createWebHistory } from 'vue-router'
-import { useTransactionStore } from '../store/useStore'
-import FormView from '../views/FormView.vue'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
-import SearchView from '../views/SearchView.vue'
+  RouteLocationNormalized,
+  NavigationGuardNext,
+} from "vue-router";
+import { useTransactionStore } from "../store/useStore";
+import FormView from "../views/FormView.vue";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
+import SearchView from "../views/SearchView.vue";
+
+const isAuthenticated = () => {
+  return !!localStorage.getItem("token");
+};
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: HomeView,
+    beforeEnter: (
+      _to: RouteLocationNormalized,
+      _from: RouteLocationNormalized,
+      next: NavigationGuardNext
+    ) => {
+      if (isAuthenticated()) {
+        next();
+      } else {
+        next("/login");
+      }
+    },
   },
   {
-    path: '/login',
-    name: 'Login',
+    path: "/login",
+    name: "Login",
     component: LoginView,
+    beforeEnter: (
+      _to: RouteLocationNormalized,
+      _from: RouteLocationNormalized,
+      next: NavigationGuardNext
+    ) => {
+      if (isAuthenticated()) {
+        next("/");
+      } else {
+        next();
+      }
+    },
   },
   {
-    path: '/register',
-    name: 'Register',
+    path: "/register",
+    name: "Register",
     component: RegisterView,
+    beforeEnter: (
+      _to: RouteLocationNormalized,
+      _from: RouteLocationNormalized,
+      next: NavigationGuardNext
+    ) => {
+      if (isAuthenticated()) {
+        next("/");
+      } else {
+        next();
+      }
+    },
   },
   {
-    path: '/transaction',
-    name: 'FormTransaction',
+    path: "/transaction",
+    name: "FormTransaction",
     component: FormView,
     beforeEnter: (
       _to: RouteLocationNormalized,
       _from: RouteLocationNormalized,
-      next: NavigationGuardNext,
+      next: NavigationGuardNext
     ) => {
-      const { transactions } = useTransactionStore()
-
-      if (transactions && transactions.length > 0) {
-        next()
-      }
-      else {
-        next('/')
+      const { transactions } = useTransactionStore();
+      if (isAuthenticated()) {
+        if (transactions && transactions.length > 0) {
+          next();
+        } else {
+          next({ name: "Home" });
+        }
+      } else {
+        next({ name: "Login" });
       }
     },
   },
   {
-    path: '/all',
-    name: 'SearchTransactions',
+    path: "/all",
+    name: "SearchTransactions",
     component: SearchView,
     beforeEnter: (
       _to: RouteLocationNormalized,
       _from: RouteLocationNormalized,
-      next: NavigationGuardNext,
+      next: NavigationGuardNext
     ) => {
-      const { transactions } = useTransactionStore()
-
-      if (transactions && transactions.length > 0) {
-        next()
-      }
-      else {
-        next('/')
+      const { transactions } = useTransactionStore();
+      if (isAuthenticated()) {
+        if (transactions && transactions.length > 0) {
+          next();
+        } else {
+          next("/");
+        }
+      } else {
+        next("/login");
       }
     },
   },
-]
+];
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
