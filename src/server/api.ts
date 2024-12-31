59e5e61b19c8a51fee0419b7b39b8fdb2abb754e
sohseyedi <sohseyedi@gmail.com>
2024-12-31 12:36:31 +0330

    feat:create an Axios instance with a base URL and timeout

---
+++ src/server/api.ts
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import { BASE_URL } from '../config'
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  error => Promise.reject(error),
+)
+
+api.interceptors.response.use(
+  response => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.log('Unauthorized! Redirecting to login...')
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  },
+)
+
+export default {
+  get: api.get,
+  post: api.post,
+  put: api.put,
+  delete: api.delete,
+}
