9d070c19593d3ea7d9ed405165bb0c7fd7bd4061
sohseyedi <sohseyedi@gmail.com>
2025-01-01 18:08:58 +0330

    style:lint app

--- src/server/urls.ts
+++ src/server/urls.ts
@@ -1,5 +1,5 @@
-export const SIGNUP_URL = "/auth/register";
-export const SIGNIN_URL = "/auth/login";
-export const GET_TRANSACTIONS_URL = "/transaction";
-export const POST_TRANSACTIONS_URL = "/transaction";
-export const DELETE_TRANSACTIONS_URL = (id: string) => `/transaction/${id}/`;
+export const SIGNUP_URL = '/auth/register'
+export const SIGNIN_URL = '/auth/login'
+export const GET_TRANSACTIONS_URL = '/transaction'
+export const POST_TRANSACTIONS_URL = '/transaction'
+export const DELETE_TRANSACTIONS_URL = (id: string) => `/transaction/${id}/`
