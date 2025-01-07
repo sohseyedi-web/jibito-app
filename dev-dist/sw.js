bc5ce926c7fa01db511af581a26557ce8ac3bbb4
sohseyedi <sohseyedi@gmail.com>
2025-01-07 19:03:43 +0330

    chore:sw pwa

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.20vijm2ru7g"
+    "revision": "0.02obuo0vjag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
